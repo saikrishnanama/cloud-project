e9a07d6bbf252c20db18b1be4562e647a84ac503
saikrishnanama <saikrishnanama99@gmail.com>
2024-09-21 15:25:47 -0500

    adding cloud project

---
+++ node.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to the Cloud!</h1>');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
